e562a68949905d791460da2f58c026d1198f214b
Pranshu Malviya <pranshumalviya2@users.noreply.github.com>
2016-04-04 01:35:44 +0530

    Update k_means.py

--- test2/k_means.py
+++ test2/k_means.py
@@ -6,8 +6,10 @@ import math
 df = pd.read_csv("household_power_consumption.txt",sep=";")
 df.as_matrix()
 
-no_p=9
-
 data=[]
-for i in range(no_p):
+for i in range(len(df.columns)):
 	data.append([t for t in df[df.columns[i]].values])
+data=np.array(data).T
+
+#print(data)
+
