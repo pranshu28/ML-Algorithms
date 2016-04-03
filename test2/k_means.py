9aa937422fb3810a70771827e3fa662bc66be951
Pranshu Malviya <pranshumalviya2@users.noreply.github.com>
2016-04-04 01:59:14 +0530

    Update k_means.py

--- test2/k_means.py
+++ test2/k_means.py
@@ -7,9 +7,20 @@ df = pd.read_csv("household_power_consumption.txt",sep=";")
 df.as_matrix()
 
 data=[]
-for i in range(len(df.columns)):
-	data.append([t for t in df[df.columns[i]].values])
-data=np.array(data).T
+for i in range(2,len(df.columns)):
+	data.append([float(t) for t in df[df.columns[i]].values])
 
-#print(data)
+d=np.array(data).T
 
+x_axis=[int(t) for t in range(len(data[0]))]
+y_axis=data[3]
+
+x_min, x_max = np.array(x_axis).min() - 1,np.array(x_axis).max()+ 1
+y_min, y_max = np.array(y_axis).min() - 1,np.array(x_axis).max()+ 1
+
+plt.scatter(x_axis,y_axis)
+
+plt.xlim(x_min,x_max)
+#plt.ylim(y_min, y_max)
+
+plt.show()
