6e2f93b3afa9428382cee4ec80fb345fc8137a24
Pranshu Malviya <pranshumalviya2@users.noreply.github.com>
2016-04-09 12:15:22 +0530

    Update em_sklearn.py

--- test2/em_sklearn.py
+++ test2/em_sklearn.py
@@ -19,7 +19,11 @@ y_min,y_max=np.array(y_axis).min()-1,np.array(y_axis).max()+1
 
 plt.scatter(x_axis,y_axis,marker='x',c='yellow')
 
-em = mixture.GMM(n_components=2, covariance_type='full')
+#number of clusters
+k=5
+
+#em implementation
+em = mixture.GMM(n_components=k, covariance_type='full')
 em.fit(d)
 x = np.linspace(x_min,x_max)
 y = np.linspace(y_min,y_max)
@@ -27,7 +31,7 @@ X, Y = np.meshgrid(x, y)
 XX = np.array([X.ravel(), Y.ravel()]).T
 Z = -em.score_samples(XX)[0]
 Z = Z.reshape(X.shape)
-CS = plt.contour(X, Y, Z,levels=np.logspace(0, 3, 10))
+CS = plt.contour(X, Y, Z,levels=np.logspace(0,2,5))
 CB = plt.colorbar(CS, shrink=0.8, extend='both')
 
 plt.xlim(x_min,x_max)
