cd156e6e4806d1cb3cf7494c7c4572e3a0373719
Pranshu Malviya <pranshumalviya2@users.noreply.github.com>
2016-06-01 10:53:15 +0530

    Update pca.py

--- test2/pca.py
+++ test2/pca.py
@@ -27,25 +27,3 @@ for i in range(k):
 W=np.array(W).T
 
 print(W)
-
-data=[]
-for i in range(no_p):
-	data.append([t for t in df[df.columns[i]].values])
-
-#Eigen values and Vectors of Covariance Matrix
-eigval,eigvec=np.linalg.eig(np.array(np.cov(np.array(data))))
-arr=[]
-for i in range(no_p):
-	arr.append([eigval[i],eigvec[i]])
-
-#No. of dimensions
-k=2
-
-#Dimensionally reduced vectors
-sort=sorted(arr,key=lambda x: x[0])[-k:]
-W=[]
-for i in range(k):
-	W.append(sort[i][1])
-W=np.array(W).T
-
-print(W)
